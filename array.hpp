cbbad874b1df7503d0c5452ae98b3a8b4e6dcc31
George G. Vega Yon <g.vegayon@gmail.com>
2020-05-08 14:58:57 -0700

    Working very fast version, swap row/col, insert/rm cell. Checking transpose (still a bit slower)

--- array.hpp
+++ array.hpp
@@ -1,9 +1,15 @@
-#ifndef ARRAY_H
-#define ARRAY_H 1
-
 #include <Rcpp.h>
 using namespace Rcpp;
 
+#ifndef ARRAY_H
+#define ARRAY_H 1
+
+
+// Constants
+#define ARRAY_CHECK_BOTH -1
+#define ARRAY_CHECK_NONE 0
+#define ARRAY_CHECK_ONE 1
+#define ARRAY_CHECK_TWO 2
 
 // Definition of the class structure
 
@@ -24,6 +30,7 @@ public:
   bool visited;
   Cell() {};
   Cell(double value_) : value(value_), visited(false) {};
+  Cell(double value_, bool visited_) : value(value_), visited(visited_) {};
   ~Cell() {};
   
   void add(double x) {this->value+=x;};
@@ -35,6 +42,13 @@ public:
   uint M;
   std::vector< umap_int_cell >  el_ij;
   std::vector< umap_int_cell_ptr > el_ji;
+  
+  // This is as a reference, if we need to iterate through the cells and we need
+  // to keep track which were visited, we use this as a reference. So that if
+  // cell.visited = true and visited = true, it means that we haven't been here
+  // yet. Ideally, any routine using this->visited should switch it at the
+  // beginning of the routine.
+  bool visited;
   
   // Empty datum
   Array (uint N_, uint M_) : N(N_), M(M_), el_ij(N_), el_ji(M_) {};
@@ -59,14 +73,21 @@ public:
   double get_cell(uint i, uint j, bool check_bounds = true) const;
   const umap_int_cell * get_row(uint i, bool check_bounds = true) const {return &(el_ij.at(i));}
   const umap_int_cell_ptr * get_col(uint i, bool check_bounds = true) const {return &(el_ji.at(i));}
+  bool is_empty(uint i, uint j, bool check_bounds = true) const;
   
   // Deletion addition operations
-  void rm_cell(uint i, uint j, bool check_bounds = true);
-  void insert_cell(uint i, uint j, double v, bool check_bounds = true);
-  void insert_cell(uint i, uint j, bool check_bounds = true);
-  void swap_cells(uint i0, uint j0, uint i1, uint j1, bool check_bounds = true);
+  void rm_cell(uint i, uint j, bool check_bounds = true, bool check_exists = true);
+  
+  void insert_cell(uint i, uint j, double v, bool check_bounds = true, bool check_exists = true);
+  void insert_cell(uint i, uint j, Cell v, bool check_bounds = true, bool check_exists = true);
+  void insert_cell(uint i, uint j, bool check_bounds = true, bool check_exists = true);
+  
+  void swap_cells(uint i0, uint j0, uint i1, uint j1, bool check_bounds = true, int check_exists = ARRAY_CHECK_BOTH);
+  
   void swap_rows(uint i0, uint i1, bool check_bounds = true);
-  void swap_col(uint j0, uint j1, bool check_bounds = true);
+  void swap_cols(uint j0, uint j1, bool check_bounds = true);
+  
+  void transpose();
   
   
 };
@@ -86,10 +107,12 @@ Array::Array (
     Rcpp::stop("Must match the size.");
   
   // Initializing
-  N = N_;
-  M = M_;
+  this->N = N_;
+  this->M = M_;
+  this->visited = false;
   el_ij.resize(N);
   el_ji.resize(M);
+  
   
   // Writing the data
   for (uint i = 0u; i < source.size(); ++i) {
@@ -111,7 +134,7 @@ Array::Array (
     }
     
     // Adding the value and creating a pointer to it
-    el_ij.at(source.at(i))[target.at(i)] = Cell(value.at(i));
+    el_ij.at(source.at(i))[target.at(i)] = Cell(value.at(i), this->visited);
     el_ji.at(target.at(i))[source.at(i)] = &el_ij.at(source.at(i))[target.at(i)];
   }
   
@@ -137,24 +160,42 @@ double Array::get_cell(uint i, uint j, bool check_bounds) const {
   
 }
 
-void Array::rm_cell(uint i, uint j, bool check_bounds) {
+bool Array::is_empty(uint i, uint j, bool check_bounds) const {
+  
+  if (check_bounds)
+    out_of_range(i, j);
+  
+  if (this->el_ij.at(i).size() == 0u)
+    return true;
+  else if (this->el_ji.at(j).size() == 0u)
+    return true;
+  
+  if (this->el_ij.at(i).find(j) == this->el_ij.at(i).end())
+    return true;
+  
+  return false;
+  
+}
+
+void Array::rm_cell(uint i, uint j, bool check_bounds, bool check_exists) {
   
   // Checking the boundaries
   if (check_bounds)
     out_of_range(i,j);
 
-  // Nothing to do
-  if (this->el_ij.at(i).size() == 0u)
-    return;
-  
-  // Checking the counter part
-  if (this->el_ji.at(j).size() == 0u)
-    return;
-  
-  // Hard work, need to remove it from both, if it exist
-  auto search = this->el_ij.at(i).find(j);
-  if (search == this->el_ij.at(i).end())
-    return;
+  if (check_exists) {
+    // Nothing to do
+    if (this->el_ij.at(i).size() == 0u)
+      return;
+    
+    // Checking the counter part
+    if (this->el_ji.at(j).size() == 0u)
+      return;
+    
+    // Hard work, need to remove it from both, if it exist
+    if (this->el_ij.at(i).find(j) == this->el_ij.at(i).end())
+      return;
+  }
   
   // Remove the pointer first (so it wont point to empty)
   this->el_ji.at(j).erase(i);
@@ -166,97 +207,200 @@ void Array::rm_cell(uint i, uint j, bool check_bounds) {
 /***
  * This member adds a new object to the edgelist
  */
-void Array::insert_cell(uint i, uint j, double v, bool check_bounds) {
+void Array::insert_cell(uint i, uint j, double v, bool check_bounds, bool check_exists) {
   
   if (check_bounds)
     out_of_range(i,j);
   
-  // Checking if nothing here, then we move along
-  if (this->el_ij.at(i).size() == 0u) {
+  if (check_exists) {
     
-    this->el_ij.at(i)[j] = Cell(v);
-    this->el_ji.at(j)[i] = &this->el_ij.at(i).at(j);
-    return;
+    // Checking if nothing here, then we move along
+    if (this->el_ij.at(i).size() == 0u) {
+      
+      this->el_ij.at(i)[j] = Cell(v);
+      this->el_ji.at(j)[i] = &this->el_ij.at(i).at(j);
+      return;
+      
+    }
     
-  }
-  
-  // In this case, the row exists, but we are checking that the value is empty
-  auto search = this->el_ij.at(i).find(j);
-  if (search == this->el_ij.at(i).end()) {
-    this->el_ij.at(i)[j] = Cell(v);
-    this->el_ji.at(j)[i] = &this->el_ij.at(i).at(j);
-  } else {
+    // In this case, the row exists, but we are checking that the value is empty
+    if (this->el_ij.at(i).find(j) == this->el_ij.at(i).end()) {
+      this->el_ij.at(i)[j] = Cell(v);
+      this->el_ji.at(j)[i] = &this->el_ij.at(i).at(j);
+    }
+    
+  } else 
     this->el_ij.at(i).at(j).add(v);
-  }
   
   return;
   
 }
 
-void Array::insert_cell(uint i, uint j, bool check_bounds) {
+void Array::insert_cell(uint i, uint j, Cell v, bool check_bounds, bool check_exists) {
   
   if (check_bounds)
     out_of_range(i,j);
   
-  // Checking if nothing here, then we move along
-  if (this->el_ij.at(i).size() == 0u) {
+  if (check_exists) {
     
-    this->el_ij.at(i)[j] = Cell(1.0);
-    this->el_ji.at(j)[i] = &this->el_ij.at(i).at(j);
-    return;
-    
-  }
+    // Checking if nothing here, then we move along
+    if (this->el_ij.at(i).size() == 0u) {
+      
+      this->el_ij.at(i)[j] = v;
+      this->el_ji.at(j)[i] = &this->el_ij.at(i).at(j);
+      return;
+      
+    }
   
-  // In this case, the row exists, but we are checking that the value is empty
-  auto search = this->el_ij.at(i).find(j);
-  if (search == this->el_ij.at(i).end()) {
-    this->el_ij.at(i)[j] = Cell(1.0);
-    this->el_ji.at(j)[i] = &this->el_ij.at(i).at(j);
+    // In this case, the row exists, but we are checking that the value is empty  
+    if (this->el_ij.at(i).find(j) == this->el_ij.at(i).end()) {
+      this->el_ij.at(i)[j] = v;
+      this->el_ji.at(j)[i] = &this->el_ij.at(i).at(j);
+    } else {
+      Rcpp::stop("Cells cannot be added together.");
+    }
+      
   } else {
-    this->el_ij.at(i).at(j).add(1.0);
+    
+    this->el_ij.at(i)[j] = v;
+    this->el_ji.at(j)[i] = &this->el_ij.at(i).at(j);
+    
   }
   
   return;
   
 }
 
-void Array::swap_cells(uint i0, uint j0, uint i1, uint j1, bool check_bounds) {
+void Array::insert_cell(uint i, uint j, bool check_bounds, bool check_exists) {
+  
+  if (check_bounds)
+    out_of_range(i,j);
+  
+  
+  if (check_exists) {
+    // Checking if nothing here, then we move along
+    if (this->el_ij.at(i).size() == 0u) {
+      
+      this->el_ij.at(i)[j] = Cell(1.0);
+      this->el_ji.at(j)[i] = &this->el_ij.at(i).at(j);
+      return;
+      
+    }
+  
+    // In this case, the row exists, but we are checking that the value is empty
+    if (this->el_ij.at(i).find(j) == this->el_ij.at(i).end()) {
+      this->el_ij.at(i)[j] = Cell(1.0);
+      this->el_ji.at(j)[i] = &this->el_ij.at(i).at(j);
+    } else
+      this->el_ij.at(i).at(j).add(1.0);
+      
+    
+  } else {
+    
+    // No checks to see if it exists, whatsoever, we just add it.
+    this->el_ij.at(i)[j] = Cell(1.0);
+    this->el_ji.at(j)[i] = &this->el_ij.at(i).at(j);
+    
+  }
+  
+  return;
+  
+}
+
+void Array::swap_cells(uint i0, uint j0, uint i1, uint j1, bool check_bounds, int check_exists) {
   
   if (check_bounds) {
     out_of_range(i0,j0);
     out_of_range(i1,j1);
   }
+  
+  // If source and target coincide, we do nothing
+  if ((i0 == i1) && (j0 == j1))
+    return;
+  
+  // Simplest case, we know both exists, so we don't need to check anything
+  if (check_exists == ARRAY_CHECK_NONE) {
+    
+    Cell c0 = this->el_ij.at(i0).at(j0);
+    this->rm_cell(i0, j0, false, false);
+    Cell c1 = this->el_ij.at(i1).at(j1);
+    this->rm_cell(i1, j1, false, false);
+    
+    this->insert_cell(i0, j0, c1, false, false);
+    this->insert_cell(i1, j1, c0, false, false);
+    
+    return;
+
+  }
     
   bool move0 = true, move1 = true;
   
   // Do we need to move 0?
-  if (this->el_ij.at(i0).size() == 0u) move0 = false;
-  else if (this->el_ji.at(j0).size() == 0u) move0 = false;
+  bool check0 = (check_exists == ARRAY_CHECK_BOTH) || (check_exists == ARRAY_CHECK_ONE);
+  bool check1 = (check_exists == ARRAY_CHECK_BOTH) || (check_exists == ARRAY_CHECK_TWO);
+  
+  if (check0) {
+    if      (this->el_ij.at(i0).size() == 0u) move0 = false;
+    else if (this->el_ji.at(j0).size() == 0u) move0 = false;
+  }
   
   // How about 1?
-  if (this->el_ij.at(i1).size() == 0u) move1 = false;
-  else if (this->el_ji.at(j1).size() == 0u) move1 = false;
+  if (check1) {
+    if      (this->el_ij.at(i1).size() == 0u) move1 = false;
+    else if (this->el_ji.at(j1).size() == 0u) move1 = false;
+  }
+  
   
   // Case 1: Both need to be moved:
   if (move0 && move1) {
     
     // Swapping elements, the pointers will work OK as the memory
     // location hasn't change, right?
-    Cell c0 = this->el_ij.at(i0).at(j0);
-    this->el_ij.at(i0).at(j0) = this->el_ij.at(i1).at(j1);
-    this->el_ij.at(i1).at(j1) = c0;
+    check0 = !check0 || (this->el_ij.at(i0).find(j0) != this->el_ij.at(i0).end());
+    check1 = !check1 || (this->el_ij.at(i1).find(j1) != this->el_ij.at(i1).end());
+    
+    // If both cells exists
+    if (check0 & check1) {
+      
+      Cell c0 = this->el_ij.at(i0).at(j0);
+      this->rm_cell(i0, j0, false, false);
+      Cell c1 = this->el_ij.at(i1).at(j1);
+      this->rm_cell(i1, j1, false, false);
+      
+      this->insert_cell(i0, j0, c1, false, false);
+      this->insert_cell(i1, j1, c0, false, false);
+      
+    } else if (!check0 & check1) { // If only the second exists
+      
+      this->insert_cell(i0, j0, this->el_ij.at(i1).at(j1), false, false);
+      this->rm_cell(i1, j1, false, false);
+      
+    } else if (check0 & !check1) {
+      
+      this->insert_cell(i1, j1, this->el_ij.at(i0).at(j0), false, false);
+      this->rm_cell(i0, j0, false, false);
+      
+    }
     
   } else if (move0 && !move1) {
     
+    check0 = !check0 || (this->el_ij.at(i0).find(j0) != this->el_ij.at(i0).end());
+    
     // Adding and removing at the same time
-    this->insert_cell(i1, j1, this->el_ij.at(i0).at(j0).value, false);
-    this->rm_cell(i0, j0, false);
+    if (check0) {
+      this->insert_cell(i1, j1, this->el_ij.at(i0).at(j0), false, false);
+      this->rm_cell(i0, j0, false, false);
+    }
     
   } else if (!move0 && move1) {
     
+    check1 = !check1 || (this->el_ij.at(i1).find(j1) != this->el_ij.at(i1).end());
+    
     // Likewise, but the other element
-    this->insert_cell(i0, j0, this->el_ij.at(i1).at(j1).value, false);
-    this->rm_cell(i1, j1, false);
+    if (check1) {
+      this->insert_cell(i0, j0, this->el_ij.at(i1).at(j1), false, false);
+      this->rm_cell(i1, j1, false, false);
+    }
     
   }
   
@@ -305,18 +449,93 @@ void Array::swap_rows(uint i0, uint i1, bool check_bounds) {
 }
 
 // This swapping is more expensive overall
-void Array::swap_col(uint j0, uint j1, bool check_bounds) {
+void Array::swap_cols(uint j0, uint j1, bool check_bounds) {
   
-  // if (check_bounds) {
-  //   out_of_range(j0,0u);
-  //   out_of_range(j1,0u);
-  // }
-  // 
-  // bool move0=true, move1=true;
-  // if (this->el_ji.at(j0).size() == 0u) move0 = false;
-  // if (this->el_ji.at(j1).size() == 0u) move1 = false;
-  // 
-  // this->swap_cells()
+  if (check_bounds) {
+    out_of_range(0u, j0);
+    out_of_range(0u, j1);
+  }
+  
+  bool move0=true, move1=true;
+  if (this->el_ji.at(j0).size() == 0u) move0 = false;
+  if (this->el_ji.at(j1).size() == 0u) move1 = false;
+
+  if (!move0 && !move1)
+    return;
+  
+  // We now need to swap the contents internally
+  if (move0 && move1) {
+    
+    // We get a copy that we use to check whether we already checked these
+    // values or not
+    umap_int_cell_ptr col0 = this->el_ji.at(j0);
+    umap_int_cell_ptr col1 = this->el_ji.at(j1);
+    for (auto col = col0.begin(); col != col0.end(); ++col) {
+      
+      // We first swap
+      this->swap_cells(col->first, j0, col->first, j1, false, ARRAY_CHECK_TWO);
+      
+      // We tagged the ones that were already swapped as done
+      if (col1.find(col->first) != col1.end())
+        col1.erase(col->first);
+
+    }
+    
+    for (auto col = col1.begin(); col != col1.end(); ++col) 
+      this->swap_cells(col->first, j1, col->first, j0, false, ARRAY_CHECK_ONE);
+    
+  }
+  
+
+  
+  return;
+}
+
+void Array::transpose() {
+  
+  // Start by flipping the switch 
+  this->visited = !this->visited;
+  
+  // Do we need to resize (increase) either?
+  if      (this->N > this->M) this->el_ji.resize(this->N);
+  else if (this->N < this->M) this->el_ij.resize(this->M);
+  
+  // uint N0 = this->N, M0 = this->M;
+
+  for (uint i = 0u; i < this->N; ++i) {
+    
+    // Do we need to move anything?
+    if (this->el_ij.at(i).size() == 0u)
+      continue;
+    
+    // We now iterate changing rows
+    umap_int_cell row = this->el_ij.at(i);
+    for (auto col = row.begin(); col != row.end(); ++col) {
+      
+      // We have not visited this yet, we need to change that
+      if (this->el_ij.at(i).at(col->first).visited != this->visited) {
+        
+        // First, swap the contents
+        this->swap_cells(i, col->first, col->first, i, false, ARRAY_CHECK_TWO);
+        
+        // Changing the switch
+        if (!this->is_empty(i, col->first, false))
+          this->el_ij.at(i).at(col->first).visited = this->visited;
+        
+        this->el_ij.at(col->first).at(i).visited = this->visited;
+        
+      }
+      
+    }
+    
+  }
+  
+  // Shreding
+  if (this->N > this->M) this->el_ij.resize(this->M);
+  else if (this->N < this->M) this->el_ji.resize(this->N);
+    
+  // Swapping the values
+  std::swap(this->N, this->M);
   
   return;
 }
--- binaryarray.cpp
+++ binaryarray.cpp
@@ -84,6 +84,20 @@ int swap_rows(SEXP x, int i0, int i1) {
   return 0;
 }
 
+// [[Rcpp::export]]
+int swap_cols(SEXP x, int j0, int j1) {
+  Rcpp::XPtr< Array > xptr(x);
+  xptr->swap_cols(j0, j1);
+  return 0;
+}
+
+// [[Rcpp::export]]
+int transpose(SEXP x) {
+  Rcpp::XPtr< Array > xptr(x);
+  xptr->transpose();
+  return 0;
+}
+
 /***R
 
 set.seed(123)
@@ -96,7 +110,7 @@ target <- sample.int(M, nedges, replace = TRUE)
 values <- runif(nedges)
 
 el <- new_Array(N, M, source - 1L, target - 1L, values)
-View(cbind(cbind(source, target) - 1, values))
+# View(cbind(cbind(source, target) - 1, values))
 
 ans <- sapply(1:nedges, function(i) get_cell(el, source[i]-1, target[i]-1))
 range(values - ans)
@@ -121,24 +135,25 @@ range(t(ans) - x_sparse)
 mean(ans)
 
 # Is it worth it?
-library(microbenchmark)
-microbenchmark(
-  Matrix = x_sparse[i_max,],
-  Array  = get_row(el, i_max - 1)
-)
-
-microbenchmark(
-  Matrix = x_sparse[,j_max],
-  Array  = get_col(el, j_max - 1)
-)
+# library(microbenchmark)
+# microbenchmark(
+#   Matrix = x_sparse[i_max,],
+#   Array  = get_row(el, i_max - 1)
+# )
+# 
+# microbenchmark(
+#   Matrix = x_sparse[,j_max],
+#   Array  = get_col(el, j_max - 1)
+# )
 # range(x_sparse[7425,] - get_row(el, 7424))
 
 # Checking removing cells
 set.seed(123)
-M <- N <- 10
-source <- sample.int(N, 5)
-target <- sample.int(N, 5)
-values <- runif(5)
+M <- N <- 1000
+nedges <- 500
+source <- sample.int(N, nedges)
+target <- sample.int(N, nedges)
+values <- runif(nedges)
 
 el2 <- new_Array(N, M, source - 1L, target - 1L, values)
 
@@ -148,12 +163,12 @@ mat1 <- as(t(sapply(1:N - 1, get_row, x = el2)), "dgCMatrix")
 
 # Removing one
 rm_cell(el2, 5, 0)
-as(t(sapply(1:N - 1, get_row, x = el2)), "dgCMatrix")
+as(t(sapply(1:N - 1, get_row, x = el2)), "dgCMatrix")[1:5, 1:5]
 
 # Adding two
 insert_cell(el2, 0, 0, 1)
 insert_cell(el2, 1, 1, 2)
-as(t(sapply(1:N - 1, get_row, x = el2)), "dgCMatrix")
+as(t(sapply(1:N - 1, get_row, x = el2)), "dgCMatrix")[1:5, 1:5]
 
 # Swapping cells
 swap_cells(el2, 0, 0, 1, 1)
@@ -161,14 +176,43 @@ swap_cells(el2, N-1, N-1, N-2, N-2)
 swap_cells(el2, 9, 3, 8, 2)
 swap_cells(el2, 9, 3, 8, 2)
 # insert_cell(el2, 1, 1, 2)
-as(t(sapply(1:N - 1, get_row, x = el2)), "dgCMatrix")
+as(t(sapply(1:N - 1, get_row, x = el2)), "dgCMatrix")[1:5, 1:5]
 
-microbenchmark(
-  swap_rows(el2, 0, 1),
-  {mat0[c(1,2),] <- mat0[c(2,1),]}
-)
+# microbenchmark(
+#   Array  = swap_rows(el2, 0, 1),
+#   Matrix = {mat0[c(1,2),] <- mat0[c(2,1),]}
+# )
+# 
+# microbenchmark(
+#   Array  = swap_cols(el2, 0, 1),
+#   Matrix = {mat0[,c(1,2)] <- mat0[,c(2,1)]}
+# )
 
 swap_rows(el2, 0, 1)
-as(t(sapply(1:N - 1, get_row, x = el2)), "dgCMatrix")
-as(sapply(1:M - 1, get_col, x = el2), "dgCMatrix") # Should be equivalent
+as(t(sapply(1:N - 1, get_row, x = el2)), "dgCMatrix")[1:5, 1:5]
+as(sapply(1:M - 1, get_col, x = el2), "dgCMatrix")[1:5, 1:5] # Should be equivalent
+
+# Checking transpose -----------------------------------------------------------
+set.seed(123)
+M <- N <- 2000
+M <- M/2
+ncells <- M
+source <- sample.int(N, ncells)
+target <- sample.int(M, ncells)
+values <- runif(ncells)
+
+el3 <- new_Array(N, M, source - 1L, target - 1L, values)
+(m0 <- as(sapply(1:M - 1, get_col, x = el3), "dgCMatrix"))[1:5, 1:5]
+
+# transpose(el3)
+# (m1 <- as(sapply(1:N - 1, get_col, x = el3), "dgCMatrix"))
+
+
+# Comparing 
+microbenchmark::microbenchmark(
+  m0 <<- t(m0),
+  transpose(el3), times = 1e3,
+  unit = "relative"
+)
+
 */
