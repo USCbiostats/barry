3c9705909aebe186bb9cd018754d2884bcd962e5
George G. Vega Yon <g.vegayon@gmail.com>
2022-06-22 14:05:17 -0600

    Working on a motif parser for DEFM

--- include/barry/models/defm.hpp
+++ include/barry/models/defm.hpp
@@ -4,8 +4,12 @@
 
 // #include "../barry.hpp"
 
+#include <iterator>
+#include <regex>
+
 #include "defm/defm-bones.hpp"
 #include "defm/defm-meat.hpp"
+#include "defm/formula-bones.hpp"
 
 
 #endif
