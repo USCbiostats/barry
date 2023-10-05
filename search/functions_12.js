924424e439e6fba8ef95afdd22eee9abd842de95
gvegayon <gvegayon@users.noreply.github.com>
2023-10-05 20:50:57 +0000

    deploy: f05adbe00ec8a8ad5e5fe0153d69c2d437930bd0

--- search/functions_12.js
+++ search/functions_12.js
@@ -3,6 +3,6 @@ var searchData=
   ['update_5fannotations_1134',['update_annotations',['../class_geese.html#aca6c81b460f41f9684004692279bd0ee',1,'Geese']]],
   ['update_5flikelihoods_1135',['update_likelihoods',['../class_model.html#aeb38a3bb76c85942db7d377df591c37e',1,'Model']]],
   ['update_5fnormalizing_5fconstant_1136',['update_normalizing_constant',['../model-meat_8hpp.html#aaa38b3c2cb53d9f51f0dffde9eaff719',1,'model-meat.hpp']]],
-  ['update_5fnormalizing_5fconstants_1137',['update_normalizing_constants',['../class_model.html#a2fbb49bd54c3631b53170ac5f17669a0',1,'Model']]],
-  ['update_5fpset_5fprobs_1138',['update_pset_probs',['../class_model.html#a459fa3d5e45569f547d344269fee1bff',1,'Model']]]
+  ['update_5fnormalizing_5fconstants_1137',['update_normalizing_constants',['../class_model.html#a083ca189d7e7ccad436070943ef26027',1,'Model']]],
+  ['update_5fpset_5fprobs_1138',['update_pset_probs',['../class_model.html#a0d6163ce073ac0cee15d03f50e9186a8',1,'Model']]]
 ];
